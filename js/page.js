4538fdac15d4a1178bedcea6ebf797986276b5ad
HanVincent <b84414@gmail.com>
2016-03-19 12:16:59 +0800

    Modify the ajax function.

--- js/page.js
+++ js/page.js
@@ -4,29 +4,29 @@ var cubeNum = 50;
 var canvas = $("#board")[0];
 
 $(document).ready(function() {
-	//Ajax決定是否收到訊息，並renew
-	$.getJSON("testdata.json", function(data){
-
-	alert(data.employees[0].firstName);
-	});
-	/*$.ajax({//success(jdata, status, xhr)
-		url: "http://172.17.183.156:3000/test.json",
-		type: "GET",
-		dataType: "json",
-		success: function(result) {
-			alert(result.id +"SUCCESS!!!");
-		},
-
-		error: function() {
-			alert("ERROR!!!");
-		}
-	});*/
-
 	//initial a 2d array for board
 	var arr_board = new Array(cubeNum * cubeLen);
 	for (var i = 0; i < cubeNum; i++)
 		for (var j = 0; j < cubeNum; j++)
 			arr_board[i, j] = false;
+
+	//Ajax決定是否收到訊息，並renew
+	/*$.getJSON("testdata.json", function(data){
+		alert(data.employees[0].firstName);
+	});*/
+	$.ajax({
+		url: "testdata.json",
+		type: "GET",
+		dataType: "json",
+		success: function(result) {
+			alert(result.id);//update發亮的位置
+		},
+
+		error: function() {
+			alert("ERROR!!!");
+		}
+	});
+
 
 	//傳入的JSON資料, update array
 
@@ -51,12 +51,12 @@ function drawBoard() {
 	ctx.fillStyle = "#7B7B7B";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	//Small Rect
+	//Cut Small Rect
 	ctx.strokeStyle = "#FFFFFF";
 	for (var i = 0; i < canvas.width; i += cubeLen) {
 		for (var j = 0; j < canvas.height; j += cubeLen) {
 			ctx.strokeRect(i, j, cubeLen, cubeLen);
-			//未來加入不同顏色的方塊(有人)
+			//if else 未來加入不同顏色的方塊(有人)
 		}
 	}
 
